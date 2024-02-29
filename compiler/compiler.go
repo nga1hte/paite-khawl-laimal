0c4dd17de99e6c52f70cf2434539120408e9809c
nga1hte <ngaihte@outlook.in>
2024-02-29 22:04:00 +0530

    added testing for code

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,1 +1,35 @@
 package compiler
+
+import (
+	"github.com/nga1hte/paite-khawl-laimal/ast"
+	"github.com/nga1hte/paite-khawl-laimal/code"
+	"github.com/nga1hte/paite-khawl-laimal/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
